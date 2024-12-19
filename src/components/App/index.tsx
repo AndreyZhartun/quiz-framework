fe158776b2c53ad36b679cf9c45ebd4e941f5d3d
Andrey Zhartun <qeetree@gmail.com>
2024-12-19 13:48:50 +0300

    каркас приложения

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import DisplayCountries from '../DisplayCountries';
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <DisplayCountries/>
+    </>
+  )
+}
+
+export default App
