933364d58aebf3ab2361de51508ba96bebbdb3f7
Andrey Zhartun <qeetree@gmail.com>
2024-12-20 22:23:56 +0300

    добавил BlueprintJS и улучшил визуал игры

--- src/features/quiz/components/QuestionView/index.tsx
+++ src/features/quiz/components/QuestionView/index.tsx
@@ -1,4 +1,6 @@
+import { Card, Section, SectionCard } from "@blueprintjs/core";
 import { QuestionViewProps } from "./types";
+import styles from "./QuestionView.module.scss";
 
 const QuestionView: React.FC<QuestionViewProps> = ({
     question: {
@@ -9,14 +11,23 @@ const QuestionView: React.FC<QuestionViewProps> = ({
     processAnswer,
 }) => {
 
-    return <div>
-        <p>Вопрос №{number}</p>
-        <p>{title}</p>
-        {answerOptions.map(({id, label}) => 
-            <button key={id} onClick={() => processAnswer(id)}>
-                {label}
-            </button>)}
-    </div>
+    return <>
+        <div className="d-flex mb-3">
+            <Card className="mr-2">{number}</Card>
+            <Card>{title}</Card>
+        </div>
+
+        <div className={styles['container']}>
+            {answerOptions.map(({id, label}) => 
+                <Card 
+                    key={id} 
+                    interactive 
+                    onClick={() => processAnswer(id)}
+                    className={styles['container-button']}>
+                    {label}
+                </Card>)}
+        </div>
+    </>
 }
 
 export default QuestionView;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import './index.scss';
 import App from './components/App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
