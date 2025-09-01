dd0fc6821563251777d81a389cfe669cf2dce6ee
Andrey Zhartun <qeetree@gmail.com>
2025-09-01 19:09:18 +0300

    Updates

---
+++ assets/allPathsLoader-BJosziL4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-N_NraE_R.js","assets/index-DBrX4SuH.js","assets/index-DeDKg4Un.js","assets/index-C463aDF_.js","assets/index-CvUdBCYx.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-C463aDF_.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-N_NraE_R.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
